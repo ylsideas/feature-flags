a536412b07e4fb39f5f3fa94a32fcf6e0aa6063e
Peter Fox <peter.fox@peterfox.me>
2025-01-21 20:32:34 +0000

    PHPStan fixes

--- src/Facades/Features.php
+++ src/Facades/Features.php
@@ -5,6 +5,7 @@ namespace YlsIdeas\FeatureFlags\Facades;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\Testing\Fakes\Fake;
 use YlsIdeas\FeatureFlags\Contracts\Features as FeaturesContract;
+use YlsIdeas\FeatureFlags\Manager;
 use YlsIdeas\FeatureFlags\Support\FeatureFake;
 
 /**
@@ -12,13 +13,13 @@ use YlsIdeas\FeatureFlags\Support\FeatureFake;
  * @method static bool accessible(string $feature)
  * @method static void turnOn(string $gateway, string $feature)
  * @method static void turnOff(string $gateway, string $feature)
- * @method static \static noBlade()
- * @method static \static noScheduling()
- * @method static \static noValidations()
- * @method static \static noCommands()
- * @method static \static noMiddlewares()
- * @method static \static noQueryBuilderMixin()
- * @method static \static configureDebugging(bool $value = true)
+ * @method static Manager noBlade()
+ * @method static Manager noScheduling()
+ * @method static Manager noValidations()
+ * @method static Manager noCommands()
+ * @method static Manager noMiddlewares()
+ * @method static Manager noQueryBuilderMixin()
+ * @method static Manager configureDebugging(bool $value = true)
  * @method static bool usesBlade()
  * @method static bool usesValidations()
  * @method static bool usesScheduling()
@@ -26,9 +27,9 @@ use YlsIdeas\FeatureFlags\Support\FeatureFake;
  * @method static bool usesMiddlewares()
  * @method static bool usesDebugging()
  * @method static bool usesQueryBuilderMixin()
- * @method static \static callOnExpiredFeatures(array $expiredFeatures, callable|null $handler = null)
- * @method static \static applyOnExpiredHandler(\YlsIdeas\FeatureFlags\Contracts\ExpiredFeaturesHandler $handler)
- * @method static \static extend(string $driver, callable $builder)
+ * @method static Manager callOnExpiredFeatures(array $expiredFeatures, callable|null $handler = null)
+ * @method static Manager applyOnExpiredHandler(\YlsIdeas\FeatureFlags\Contracts\ExpiredFeaturesHandler $handler)
+ * @method static Manager extend(string $driver, callable $builder)
  * @method static \YlsIdeas\FeatureFlags\Support\MaintenanceRepository maintenanceMode()
  * @method static void assertAccessed(string $feature, int|null $count = null, string $message = '')
  * @method static void assertNotAccessed(string $feature, string $message = '')
